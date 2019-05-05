48cee5f849e7f2f7ecc378e4d61b5117d04897d6
Robert170 <42983312+Robert170@users.noreply.github.com>
2019-05-04 21:17:14 -0500

    Multiplicar por 10

--- Parcial_1/Practicas/Multiplicacionx10/Multiplicacionx10.cpp
+++ Parcial_1/Practicas/Multiplicacionx10/Multiplicacionx10.cpp
@@ -1,15 +1,16 @@
+//Realiza una función que reciba como argumento un numero entero y devuelva el numero multiplicado por diez
 #include <iostream>
 
 int main()
 {
 	//Declaramos la variable y le pedimos al usuario que ingrese un numero
-	unsigned int Numero;
+	int Numero;
 	std::cout << "introduce un numero y lo multiplicaremos por 10: " << std::endl;
 	std::cin >> Numero;
 
-	Numero = Numero & 1;
 
-	//Numero=Numero+Numero+Numero+Numero+Numero+Numero+Numero+Numero+Numero+Numero;
+	std::cout << "Tu numero  multiplicado por diez es: "<< Numero << "0" << std::endl;
+
 	std::cin.get();
 	std::cin.ignore();
 	return 0;

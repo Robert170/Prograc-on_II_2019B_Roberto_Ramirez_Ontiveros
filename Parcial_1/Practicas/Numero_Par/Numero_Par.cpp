80efb35b69b92415955d7a85c464e661944fa450
Robert170 <42983312+Robert170@users.noreply.github.com>
2019-05-06 11:49:50 -0500

    par

--- Parcial_1/Practicas/Numero_Par/Numero_Par.cpp
+++ Parcial_1/Practicas/Numero_Par/Numero_Par.cpp
@@ -2,25 +2,46 @@
 
 #include <iostream>
 
-int main()
-{
-	//Declaramos la variable y le pedimos al usuario que ingrese un numero
-	unsigned int Numero;
-	std::cout << "introduce un numero y te diremos si es par o impar: " << std::endl;
-	std::cin >> Numero;
+int Par(int Numero);
 
+int Par(int Numero)
+{
 	//Cambia el valor del numero a 0 o 1, si da 0 es par de lo contrario es impar
 	Numero = Numero & 1;
 
 	//Vemos que resultado nos dio para saber si es par o impar
 	if (Numero == 0)
 	{
-		std::cout<<"Tu numero es par"<<std::endl;
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+
+}
+
+
+int main()
+{
+	//Declaramos la variable y le pedimos al usuario que ingrese un numero
+	unsigned int Numero;
+
+	std::cout << "introduce un numero y te diremos si es par o impar: " << std::endl;
+	std::cin >> Numero;
+
+	Numero = Par(Numero);
+
+	if (Numero == 0)
+	{
+		std::cout << "Tu numero es par" << std::endl;
 	}
 	else
 	{
 		std::cout << "Tu numero es imapar" << std::endl;
 	}
+
+	
 
 	std::cin.get();
 	std::cin.ignore();
